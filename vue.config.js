3c5d24003a8ec7deaa8b3f3050f6cddb4bf0a656
zhouxinyong <zhoudaxia5683@163.com>
2018-07-06 18:44:34 +0800

    修改打包路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   runtimeCompiler: true,
   lintOnSave: true,
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/cli3-demo/'
+    : '/',
   devServer: {
     https: true
   }
